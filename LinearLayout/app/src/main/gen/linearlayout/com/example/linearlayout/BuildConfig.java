accc6a4d2409b6e5ff74dbccd6b36ebb5ac09b90
amatanat <a_metanet@hotmail.com>
2016-12-14 20:56:36 +0100

    Linear layout

---
+++ LinearLayout/app/src/main/gen/linearlayout/com/example/linearlayout/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package linearlayout.com.example.linearlayout;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
