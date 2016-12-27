4fb7a8951948ec82a9ada729b35e6be6798db6a5
amatanat <a_metanet@hotmail.com>
2016-12-27 20:54:31 +0100

    update and add low-pass & high-pass filters

--- SensorAccelerometer/app/src/main/java/sensors/com/example/sensoraccelerometer/MainActivity.java
+++ SensorAccelerometer/app/src/main/java/sensors/com/example/sensoraccelerometer/MainActivity.java
@@ -10,12 +10,18 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
-    TextView mTextView1;
-    TextView mTextView2;
-    TextView mTextView3;
+    private TextView mTextView1, mTextView2, mTextView3,
+                     mLowPass1, mLowPass2,mLowPass3,
+                     mHighPass1, mHighPass2, mHighPass3;
+
+
+    private float[] mGravity;
+    private float[] mAccel;
+
     SensorManager mSensorManager;
     Sensor mSensor;
 
+    private final float mAlpha = 0.8f;
     private static final int THRESHOLD = 400;
     private long mPrevious;
 
@@ -27,6 +33,14 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         mTextView1 = (TextView) findViewById(R.id.x_axis);
         mTextView2 = (TextView) findViewById(R.id.y_axis);
         mTextView3 = (TextView) findViewById(R.id.z_axis);
+
+        mLowPass1 = (TextView) findViewById(R.id.low_pass_x);
+        mLowPass2 = (TextView) findViewById(R.id.low_pass_y);
+        mLowPass3 = (TextView) findViewById(R.id.low_pass_z);
+
+        mHighPass1 = (TextView) findViewById(R.id.high_pass_x);
+        mHighPass2 = (TextView) findViewById(R.id.high_pass_y);
+        mHighPass3 = (TextView) findViewById(R.id.high_pass_z);
 
         mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
@@ -55,6 +69,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     @Override
     public void onSensorChanged(SensorEvent sensorEvent){
         if (sensorEvent.sensor.getType() == Sensor.TYPE_ACCELEROMETER){
+
             long mActual = System.currentTimeMillis();
             if (mActual - mPrevious > THRESHOLD){
                 mPrevious = mActual;
@@ -63,9 +78,28 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                 float yAxis = sensorEvent.values[1];
                 float zAxis = sensorEvent.values[2];
 
+                mGravity = new float[3];
+                mAccel = new float[3];
+
+                mGravity[0] = lowPass(xAxis, mGravity[0]);
+                mGravity[1] = lowPass(yAxis, mGravity[1]);
+                mGravity[2] = lowPass(zAxis, mGravity[2]);
+
+                mAccel[0] = highPass(xAxis, mGravity[0]);
+                mAccel[1] = highPass(yAxis, mGravity[1]);
+                mAccel[2] = highPass(zAxis, mGravity[2]);
+
                 mTextView1.setText(String.valueOf(xAxis));
                 mTextView2.setText(String.valueOf(yAxis));
                 mTextView3.setText(String.valueOf(zAxis));
+
+                mLowPass1.setText(String.valueOf(mGravity[0]));
+                mLowPass2.setText(String.valueOf(mGravity[1]));
+                mLowPass3.setText(String.valueOf(mGravity[2]));
+
+                mHighPass1.setText(String.valueOf(mAccel[0]));
+                mHighPass2.setText(String.valueOf(mAccel[1]));
+                mHighPass3.setText(String.valueOf(mAccel[2]));
             }
         }
     }
@@ -73,5 +107,13 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     @Override
     public void onAccuracyChanged(Sensor sensor, int type){
         //
+    }
+
+    private float lowPass(float current, float gravity){
+        return gravity * mAlpha + current * (1 - mAlpha);
+    }
+
+    private float highPass(float current, float gravity){
+        return current - gravity;
     }
 }
